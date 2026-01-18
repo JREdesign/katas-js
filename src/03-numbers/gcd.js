d6e3ebeedc0819c391dd06e2c22b9dc25269f5a9
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-01-18 11:32:42 +0100

    feat(numbers): añadir kata gcd con tests

---
+++ src/03-numbers/gcd.js
@@ -0,0 +1,10 @@
+// KATAS-JS/src/03-numbers/gcd.js
+export function gcd(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    throw new TypeError("gcd: a y b deben ser enteros");
+  }
+  a = Math.abs(a);
+  b = Math.abs(b);
+  }
+  return a;
+}
