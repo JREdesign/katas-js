7a355b93865f714a344333c8aac2d9d6fc0512f0
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-01-22 11:42:14 +0100

    refactor(arrays): simplificar compact-array y mejorar mensaje de error

--- src/02-arrays/compact-array.js
+++ src/02-arrays/compact-array.js
@@ -1,6 +1,6 @@
 export function compactArray(arr) {
   if (!Array.isArray(arr)) {
-    throw new TypeError("compactArray: arr debe ser un array");
+    throw new TypeError("compactArray: el argumento debe ser un array");
   }
   return arr.filter(Boolean);
 }
