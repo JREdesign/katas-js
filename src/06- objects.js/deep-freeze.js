286b55fd277ecc9db01074c5713f256b18733d96
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-01-23 12:22:50 +0100

    Update deep-freeze.js

--- src/06- objects.js/deep-freeze.js
+++ src/06- objects.js/deep-freeze.js
@@ -5,3 +5,12 @@ export function deepFreeze(obj) {
   }
 
   Object.freeze(obj);
+  for (const key of Object.keys(obj)) {
+    const value = obj[key];
+    if (value && typeof value === "object" && !Object.isFrozen(value)) {
+      deepFreeze(value);
+    }
+  }
+
+  return obj;
+}
