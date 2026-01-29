78ad4833ee4c391755afc49235b68c443a6338e0
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-01-29 11:04:54 +0100

    feat(arrays): añadir kata flatten-one-level con tests

---
+++ src/02-arrays/flatten-one-level.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { flattenOneLevel } from "./flatten-one-level.js";
+
+describe("flattenOneLevel", () => {
+  it("aplana un nivel", () => {
+    expect(flattenOneLevel([1, [2, 3], 4])).toEqual([1, 2, 3, 4]);
+  });
+
+  it("no aplana niveles más profundos", () => {
+    expect(flattenOneLevel([1, [2, [3]], 4])).toEqual([1, 2, [3], 4]);
+  });
