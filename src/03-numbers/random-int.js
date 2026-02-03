ec9d6405a5c63b64e158c01f03bf96a636b70c55
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-02-03 21:11:11 +0100

    feat(numbers): añadir kata random-int con tests

--- src/03-numbers/random-int.js
+++ src/03-numbers/random-int.js
@@ -3,5 +3,7 @@ export function randomInt(min, max) {
     throw new TypeError("randomInt: min y max deben ser enteros");
   }
   if (min > max) {
+    throw new RangeError("randomInt: min no puede ser mayor que max");
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
