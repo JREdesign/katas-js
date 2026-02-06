775e59b3a837ce15899849f2d1e77e798277745e
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-02-06 11:03:22 +0100

    Add key validation and output object in pick function

--- src/06- objects.js/pick.js
+++ src/06- objects.js/pick.js
@@ -5,3 +5,12 @@ export function pick(obj, keys) {
   if (!Array.isArray(keys)) {
     throw new TypeError("pick: keys debe ser un array");
   }
+const out = {};
+  for (const k of keys) {
+    if (typeof k !== "string") {
+      throw new TypeError("pick: cada key debe ser un string");
+    }
+    if (k in obj) out[k] = obj[k];
+  }
+  return out;
+}
