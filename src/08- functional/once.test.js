f79b25ca44238949a84ff17c38a84c1eff94e5e4
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-02-23 12:18:16 +0100

    test(functional): tests para once

--- src/08- functional/once.test.js
+++ src/08- functional/once.test.js
@@ -5,9 +5,13 @@ describe("once", () => {
   it("solo ejecuta una vez", () => {
     const fn = vi.fn(x => x + 1);
     const o = once(fn);
-expect(o(1)).toBe(2);
+
+    expect(o(1)).toBe(2);
     expect(o(10)).toBe(2);
     expect(fn).toHaveBeenCalledTimes(1);
   });
 
-
+  it("error si no es función", () => {
+    expect(() => once(1)).toThrow();
+  });
+});
