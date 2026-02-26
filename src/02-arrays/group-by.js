6d0ce680ce727d83334dfa5966410a227a1556f2
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-02-26 13:19:24 +0100

    Update group-by.js

--- src/02-arrays/group-by.js
+++ src/02-arrays/group-by.js
@@ -1,1 +1,15 @@
+export function groupBy(arr, fn) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("groupBy: arr debe ser un array");
+  }
+  if (typeof fn !== "function") {
+    throw new TypeError("groupBy: fn debe ser función");
+  }
 
+  return arr.reduce((acc, item) => {
+    const key = fn(item);
+    if (!acc[key]) acc[key] = [];
+    acc[key].push(item);
+    return acc;
+  }, {});
+}
