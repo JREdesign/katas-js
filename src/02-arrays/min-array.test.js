790c063b60ff760cc04638b71b04b60c9d6549c8
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-13 14:16:54 +0100

    Add tests for minArray function error handling

--- src/02-arrays/min-array.test.js
+++ src/02-arrays/min-array.test.js
@@ -6,3 +6,11 @@ describe("minArray", () => {
     expect(minArray([1, 3, 2])).toBe(1);
     expect(minArray([-10, -3, -7])).toBe(-10);
   });
+it("lanza error si está vacío", () => {
+    expect(() => minArray([])).toThrow();
+  });
+
+  it("lanza error si hay elementos no válidos", () => {
+    expect(() => minArray([1, "2"])).toThrow();
+    expect(() => minArray([1, NaN])).toThrow();
+  });
