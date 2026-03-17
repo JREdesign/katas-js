2666aa6062cce0c9cc2feff187dc72880d79b174
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-17 13:01:43 +0100

    Add tests for average function error handling

--- src/03-numbers/average.test.js
+++ src/03-numbers/average.test.js
@@ -6,3 +6,11 @@ describe("average", () => {
     expect(average([1, 2, 3])).toBe(2);
     expect(average([10, 20])).toBe(15);
   });
+it("lanza error si está vacío", () => {
+    expect(() => average([])).toThrow();
+  });
+
+  it("lanza error si hay elementos no válidos", () => {
+    expect(() => average([1, "2"])).toThrow();
+    expect(() => average([1, NaN])).toThrow();
+  });
