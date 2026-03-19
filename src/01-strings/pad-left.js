2c60bce50c1622664f0ebc66c5bb20ccf26b60c3
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-19 11:17:49 +0100

    Update pad-left.js

--- src/01-strings/pad-left.js
+++ src/01-strings/pad-left.js
@@ -2,3 +2,6 @@ export function padLeft(input, length, char = " ") {
   if (typeof input !== "string") {
     throw new TypeError("padLeft: input debe ser un string");
   }
+if (!Number.isInteger(length) || length < 0) {
+    throw new TypeError("padLeft: length debe ser un entero >= 0");
+}
