d81344a422f3f83b1e9d6970c38b77bf56dad141
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-23 12:22:46 +0100

    Update pad-left.test.js

--- src/02-arrays/pad-left.test.js
+++ src/02-arrays/pad-left.test.js
@@ -1,1 +1,7 @@
+import { describe, it, expect } from "vitest";
+import { padLeft } from "./pad-left.js";
 
+describe("padLeft", () => {
+  it("rellena a la izquierda con espacios por defecto", () => {
+    expect(padLeft("a", 3)).toBe("  a");
+  });
