552c70b4f8cbbb31a31064c395e193d323121b13
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-25 17:12:13 +0100

    Update starts-with-ci.js

--- src/01-strings/starts-with-ci.js
+++ src/01-strings/starts-with-ci.js
@@ -1,1 +1,6 @@
-
+export function startsWithCI(input, prefix) {
+  if (typeof input !== "string" || typeof prefix !== "string") {
+    throw new TypeError("startsWithCI: input y prefix deben ser strings");
+  }
+  return input.toLowerCase().startsWith(prefix.toLowerCase());
+}
