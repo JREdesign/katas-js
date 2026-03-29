32c5fc76e359345b4450b0d31f312d0cdbe8c1f1
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-29 18:27:31 +0200

    Update pluck 1.js

--- src/02-arrays/pluck.js
+++ src/02-arrays/pluck.js
@@ -2,3 +2,6 @@ export function pluck(arr, key) {
   if (!Array.isArray(arr)) {
     throw new TypeError("pluck: arr debe ser un array");
   }
+  if (typeof key !== "string") {
+    throw new TypeError("pluck: key debe ser un string");
+  }
