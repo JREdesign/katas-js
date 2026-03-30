fbb810122778e6756ff69b1800f506d5c266b3a3
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-03-30 10:21:00 +0200

    Update pluck.test.js

--- src/02-arrays/pluck.test.js
+++ src/02-arrays/pluck.test.js
@@ -1,1 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { pluck } from "./pluck.js";
 
+describe("pluck", () => {
+  it("extrae valores de una clave", () => {
+    const users = [{ name: "A" }, { name: "B" }];
+    expect(pluck(users, "name")).toEqual(["A", "B"]);
+  });
