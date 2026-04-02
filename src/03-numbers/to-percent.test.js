e0e0d13ae6376e9b39b48b6764dcf93ab78b5963
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-02 12:39:19 +0200

    Add test 2 for rounding behavior in toPercent function

--- src/03-numbers/to-percent.test.js
+++ src/03-numbers/to-percent.test.js
@@ -6,3 +6,6 @@ describe("toPercent", () => {
     expect(toPercent(0.5)).toBe(50);
     expect(toPercent(0.1234, 2)).toBe(12.34);
   });
+  it("redondea según decimales", () => {
+    expect(toPercent(0.1299, 1)).toBe(13);
+  });
