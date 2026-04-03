764c572d123f9f687543e2cbdac6e16e48e79d77
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-03 10:05:43 +0200

    feat(objects): añadir safe-get
    Add type check for key parameter in safeGet function.

--- src/06- objects.js/safe-get.js
+++ src/06- objects.js/safe-get.js
@@ -2,3 +2,8 @@ export function safeGet(obj, key, defaultValue = undefined) {
   if (obj === null || typeof obj !== "object") {
     return defaultValue;
   }
+ if (typeof key !== "string") {
+    throw new TypeError("safeGet: key debe ser un string");
+  }
+  return key in obj ? obj[key] : defaultValue;
+}
