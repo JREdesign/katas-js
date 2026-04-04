0c1f559cfd9533a342481d82b308ec20e6e389ce
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-04 22:36:54 +0200

    feat(objects): añadir test para safe-get

--- src/06- objects.js/safe-get.test.js
+++ src/06- objects.js/safe-get.test.js
@@ -8,5 +8,12 @@ describe("safeGet", () => {
     expect(safeGet(obj, "nombre")).toBe("Jorge");
     expect(safeGet(obj, "edad")).toBe(30);
   });
+  it("devuelve el valor por defecto si la clave no existe o si obj no es un objeto válido", () => {
+    const obj = { nombre: "Jorge" };
 
+    expect(safeGet(obj, "apellido", "Desconocido")).toBe("Desconocido");
+    expect(safeGet(null, "nombre", "Sin valor")).toBe("Sin valor");
+    expect(safeGet(123, "nombre", "Sin valor")).toBe("Sin valor");
+  });
+});
 
