1dd4bfd9b1124065ff792573ce04c2005886bc4b
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-06 18:19:30 +0200

    Implement endsWithCI function with type checking
    Add endsWithCI function to check string suffix

--- src/01-strings/ends-with-ci.js
+++ src/01-strings/ends-with-ci.js
@@ -1,1 +1,4 @@
-
+export function endsWithCI(input, suffix) {
+  if (typeof input !== "string" || typeof suffix !== "string") {
+    throw new TypeError("endsWithCI: input y suffix deben ser strings");
+  }
