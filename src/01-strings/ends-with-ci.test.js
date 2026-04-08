d23bed99b04615ff4f34c92a43aa08e69800c364
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-08 22:29:01 +0200

    Add tests for endsWithCI function
    Add tests for endsWithCI function to check case insensitivity.

--- src/01-strings/ends-with-ci.test.js
+++ src/01-strings/ends-with-ci.test.js
@@ -1,1 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { endsWithCI } from "./ends-with-ci.js";
 
+describe("endsWithCI", () => {
+  it("comprueba sufijo sin importar mayúsculas", () => {
+    expect(endsWithCI("Hola Mundo", "DO")).toBe(true);
+    expect(endsWithCI("Hola Mundo", "do")).toBe(true);
+  });
