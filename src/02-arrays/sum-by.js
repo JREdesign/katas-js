ff876fb361dcc94eec681675e1215fc10a39826b
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-11 11:00:28 +0200

    Validate input types in sumBy function
    Add type checks for input parameters in sumBy function.

--- src/02-arrays/sum-by.js
+++ src/02-arrays/sum-by.js
@@ -2,3 +2,6 @@ export function sumBy(arr, fn) {
   if (!Array.isArray(arr)) {
     throw new TypeError("sumBy: arr debe ser un array");
   }
+  if (typeof fn !== "function") {
+    throw new TypeError("sumBy: fn debe ser función");
+  }
