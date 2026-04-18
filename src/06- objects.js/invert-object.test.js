ec8891860cdf55fdf98396d948a33abf59f11170
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-18 11:52:47 +0200

    Add test for invertObject function

--- src/06- objects.js/invert-object.test.js
+++ src/06- objects.js/invert-object.test.js
@@ -1,3 +1,8 @@
 import { describe, it, expect } from "vitest";
 import { invertObject } from "./invert-object.js";
 
+describe("invertObject", () => {
+  it("invierte claves y valores", () => {
+    expect(invertObject({ a: 1, b: 2 })).toEqual({ "1": "a", "2": "b" });
+  });
+
