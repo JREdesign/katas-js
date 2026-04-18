22702945172d41dcfd186222b3341cbf99e842fd
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-18 11:53:02 +0200

    Add test for handling duplicate values in invertObject

--- src/06- objects.js/invert-object.test.js
+++ src/06- objects.js/invert-object.test.js
@@ -6,3 +6,6 @@ describe("invertObject", () => {
     expect(invertObject({ a: 1, b: 2 })).toEqual({ "1": "a", "2": "b" });
   });
 
+  it("si hay valores duplicados, prevalece la última clave", () => {
+    expect(invertObject({ a: 1, b: 1 })).toEqual({ "1": "b" });
+  });
