183f40b1ff4f3b6e9b87aeb889a56bcf3090c791
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-22 17:35:27 +0200

    Add character counting logic to countChar function

--- src/01-strings/count-char.js
+++ src/01-strings/count-char.js
@@ -5,3 +5,7 @@ export function countChar(input, char) {
   if (typeof char !== "string" || char.length !== 1) {
     throw new TypeError("countChar: char debe ser un string de longitud 1");
   }
+  let count = 0;
+  for (const current of input) {
+    if (current === char) count += 1;
+  }
