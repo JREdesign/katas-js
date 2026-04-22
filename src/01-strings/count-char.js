cb59fc00b9f9937b2c82dfbde7091f39d5f4992e
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-22 17:35:13 +0200

    Add validation for char parameter in countChar

--- src/01-strings/count-char.js
+++ src/01-strings/count-char.js
@@ -2,3 +2,6 @@ export function countChar(input, char) {
   if (typeof input !== "string") {
     throw new TypeError("countChar: input debe ser un string");
   }
+  if (typeof char !== "string" || char.length !== 1) {
+    throw new TypeError("countChar: char debe ser un string de longitud 1");
+  }
