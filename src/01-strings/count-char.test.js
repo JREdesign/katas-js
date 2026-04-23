c35bc71f5d109ad9d7d4d7c3c53acf5653330857
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-23 17:52:29 +0200

    Add test for character not found case

--- src/01-strings/count-char.test.js
+++ src/01-strings/count-char.test.js
@@ -6,3 +6,6 @@ describe("countChar", () => {
     expect(countChar("banana", "a")).toBe(3);
     expect(countChar("banana", "b")).toBe(1);
   });
+  it("devuelve 0 si no aparece", () => {
+    expect(countChar("hola", "z")).toBe(0);
+  });
