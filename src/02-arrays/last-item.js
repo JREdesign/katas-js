25991409a6d8701ad279a6afd33cc91c0190e85b
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-04-26 11:53:56 +0200

    Return last item or undefined for empty array

--- src/02-arrays/last-item.js
+++ src/02-arrays/last-item.js
@@ -2,3 +2,5 @@ export function lastItem(arr) {
   if (!Array.isArray(arr)) {
     throw new TypeError("lastItem: arr debe ser un array");
   }
+  return arr.length === 0 ? undefined : arr[arr.length - 1];
+}
