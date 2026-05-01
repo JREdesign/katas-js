de49740748e3bc9db0ce9e486d33a54661aab7ad
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-01 10:54:31 +0200

    Add tests for mapValues function

--- src/06- objects.js/map-values.test.js
+++ src/06- objects.js/map-values.test.js
@@ -1,3 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { mapValues } from "./map-values.js";
 
+describe("mapValues", () => {
+  it("transforma los valores manteniendo claves", () => {
+    expect(mapValues({ a: 1, b: 2 }, n => n * 2)).toEqual({ a: 2, b: 4 });
+  });
