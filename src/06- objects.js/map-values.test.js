05c5e53dda786f05695d13a71cc34f62bd06d9b8
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-01 10:56:40 +0200

    Add test for mapValues with key parameter

--- src/06- objects.js/map-values.test.js
+++ src/06- objects.js/map-values.test.js
@@ -5,3 +5,6 @@ describe("mapValues", () => {
   it("transforma los valores manteniendo claves", () => {
     expect(mapValues({ a: 1, b: 2 }, n => n * 2)).toEqual({ a: 2, b: 4 });
   });
+  it("recibe también la clave", () => {
+    expect(mapValues({ a: 1 }, (value, key) => `${key}:${value}`)).toEqual({ a: "a:1" });
+  });
