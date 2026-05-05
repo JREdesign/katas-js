b58a73e39c3a7d05d1e9b1c8d68266060a088129
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-05 17:40:19 +0200

    Add tests for removeSpaces function

--- src/01-strings/remove-spaces.test.js
+++ src/01-strings/remove-spaces.test.js
@@ -1,3 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { removeSpaces } from "./remove-spaces.js";
 
+describe("removeSpaces", () => {
+  it("elimina espacios normales", () => {
+    expect(removeSpaces("hola mundo")).toBe("holamundo");
+  });
