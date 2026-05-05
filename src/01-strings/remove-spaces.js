1d87ca2e241f9fa34466422ac587c9f5d82210a8
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-05 17:35:11 +0200

    feat(strings): añadir remove-spaces

--- src/01-strings/remove-spaces.js
+++ src/01-strings/remove-spaces.js
@@ -2,4 +2,5 @@ export function removeSpaces(input) {
   if (typeof input !== "string") {
     throw new TypeError("removeSpaces: input debe ser un string");
   }
-
+  return input.replace(/\s+/g, "");
+}
