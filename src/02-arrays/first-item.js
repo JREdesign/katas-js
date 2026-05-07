30c167336cc170b1967c58aff29901f1114311d0
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-07 17:22:17 +0200

    Return undefined for empty array in firstItem function

--- src/02-arrays/first-item.js
+++ src/02-arrays/first-item.js
@@ -2,4 +2,5 @@ export function firstItem(arr) {
   if (!Array.isArray(arr)) {
     throw new TypeError("firstItem: arr debe ser un array");
   }
-
+  return arr.length === 0 ? undefined : arr[0];
+}
