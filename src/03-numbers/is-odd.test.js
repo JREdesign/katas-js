873f4dd8fcf8dc7da08bb49ce874fad03c984297
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-10 11:27:07 +0200

    Add test cases for even numbers in isOdd function

--- src/03-numbers/is-odd.test.js
+++ src/03-numbers/is-odd.test.js
@@ -7,3 +7,9 @@ describe("isOdd", () => {
     expect(isOdd(7)).toBe(true);
     expect(isOdd(-3)).toBe(true);
   });
+
+  it("devuelve false para pares", () => {
+    expect(isOdd(0)).toBe(false);
+    expect(isOdd(2)).toBe(false);
+    expect(isOdd(-4)).toBe(false);
+  });
