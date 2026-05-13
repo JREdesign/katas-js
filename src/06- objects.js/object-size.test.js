a9ee5cdf5dbc87b38b5c893449db82d23ce557c4
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-13 17:40:10 +0200

    Add tests for objectSize function

--- src/06- objects.js/object-size.test.js
+++ src/06- objects.js/object-size.test.js
@@ -1,2 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { objectSize } from "./object-size.js";
+
+describe("objectSize", () => {
+  it("cuenta claves propias enumerables", () => {
+    expect(objectSize({ a: 1, b: 2 })).toBe(2);
+  });
