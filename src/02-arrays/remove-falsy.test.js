69b708eb212c0620190dc6052836eb157a501397
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-19 22:35:38 +0200

    Add test for empty arrays and objects in removeFalsy

--- src/02-arrays/remove-falsy.test.js
+++ src/02-arrays/remove-falsy.test.js
@@ -5,3 +5,7 @@ describe("removeFalsy", () => {
   it("elimina valores falsy", () => {
     expect(removeFalsy([0, 1, false, 2, "", 3, null, undefined, NaN])).toEqual([1, 2, 3]);
   });
+  
+  it("mantiene arrays y objetos vacíos porque son truthy", () => {
+    expect(removeFalsy([{}, [], "ok"])).toEqual([{}, [], "ok"]);
+  });
