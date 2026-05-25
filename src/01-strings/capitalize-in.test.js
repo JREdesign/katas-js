a8ed983be4f4b38e0a1a26590f4b0709a6f13d2a
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-05-25 23:14:20 +0200

    Add tests for chunkArray function

--- src/01-strings/capitalize-in.test.js
+++ src/01-strings/capitalize-in.test.js
@@ -1,1 +1,7 @@
+import { describe, it, expect } from "vitest";
+import { chunkArray } from "./chunk-array.js";
 
+describe("chunkArray", () => {
+  it("divide un array en grupos del tamaño indicado", () => {
+    expect(chunkArray([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+  });
