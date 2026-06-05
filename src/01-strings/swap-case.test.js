e08d8ccfcb2a9e0a3e5225a17d74a5cb229e7a9a
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-05 17:26:10 +0200

    Add test for swapCase function

--- src/01-strings/swap-case.test.js
+++ src/01-strings/swap-case.test.js
@@ -1,2 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { swapCase } from "./swap-case.js";
+
+describe("swapCase", () => {
+  it("intercambia mayúsculas y minúsculas", () => {
+    expect(swapCase("Hola Mundo")).toBe("hOLA mUNDO");
+  });
