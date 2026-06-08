e3cd1c2b7708962af978f13a7792ef269a0a909f
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-08 17:12:12 +0200

    Add test for dropping multiple elements from array

--- src/02-arrays/drop.test.js
+++ src/02-arrays/drop.test.js
@@ -5,3 +5,6 @@ describe("drop", () => {
   it("elimina el primer elemento por defecto", () => {
     expect(drop([1, 2, 3])).toEqual([2, 3]);
   });
+  it("elimina count elementos", () => {
+    expect(drop([1, 2, 3, 4], 2)).toEqual([3, 4]);
+  });
