764bd1202c2655c12b816113994fcaa9d0d597be
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-09 17:45:37 +0200

    final tests

--- src/03-numbers/cube.test.js
+++ src/03-numbers/cube.test.js
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { cube } from "./cube.js";
 
-describe("cube", () => {
+ describe("cube", () => {
   it("calcula el cubo de números positivos", () => {
     expect(cube(2)).toBe(8);
     expect(cube(3)).toBe(27);
@@ -15,7 +15,7 @@ describe("cube", () => {
     expect(cube(0)).toBe(0);
   });
 
-it("lanza error si no es number válido", () => {
+ it("lanza error si no es number válido", () => {
     expect(() => cube("2")).toThrow();
     expect(() => cube(NaN)).toThrow();
   });
@@ -25,10 +25,13 @@ it("lanza error si no es number válido", () => {
     expect(cube(0.5)).toBeCloseTo(0.125);
   });
 
-    it("calcula correctamente el cubo de números decimales negativos", () => {
+  it("calcula correctamente el cubo de números decimales negativos", () => {
     expect(cube(-1.5)).toBeCloseTo(-3.375);
     expect(cube(-0.5)).toBeCloseTo(-0.125);
   });
 
-
+  it("calcula correctamente el cubo de 1 y -1", () => {
+    expect(cube(1)).toBe(1);
+    expect(cube(-1)).toBe(-1);
+  });
 });
