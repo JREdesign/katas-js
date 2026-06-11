b55ed6539d12b41a3c5bde8a514947ec22f431e0
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-11 18:22:13 +0200

    feat(objects): añadir keys-to-array
    Added return statement to output keys of the object.

--- src/06- objects.js/keys-to-array.js
+++ src/06- objects.js/keys-to-array.js
@@ -2,4 +2,5 @@ export function keysToArray(obj) {
   if (obj === null || typeof obj !== "object" || Array.isArray(obj)) {
     throw new TypeError("keysToArray: obj debe ser un objeto simple");
   }
-
+  return Object.keys(obj);
+}
