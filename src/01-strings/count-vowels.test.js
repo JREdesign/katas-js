6930a6e9626bcf443cea41e1a1cac2e09ed5f3b2
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-17 17:25:36 +0200

    Add test for countVowels function

--- src/01-strings/count-vowels.test.js
+++ src/01-strings/count-vowels.test.js
@@ -1,2 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { countVowels } from "./count-vowels.js";
+
+describe("countVowels", () => {
+  it("cuenta las vocales de un texto", () => {
+    expect(countVowels("hola mundo")).toBe(4);
+  });
