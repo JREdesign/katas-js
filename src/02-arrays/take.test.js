02a0430fc8f82ab4bb4d137545bdfecf00d91cb4
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-20 11:57:29 +0200

    Add test for take function default behavior

--- src/02-arrays/take.test.js
+++ src/02-arrays/take.test.js
@@ -1,2 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { take } from "./take.js";
+
+describe("take", () => {
+  it("devuelve el primer elemento por defecto", () => {
+    expect(take([1, 2, 3])).toEqual([1]);
+  });
