099fb0507ddbaa1e4fd81a98f5a9c49c8907ccb3
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-06-26 17:05:00 +0200

    Validate predicate type in filterObject function
    Add type checks for predicate parameter in filterObject function.

--- src/06- objects.js/filter-object.js
+++ src/06- objects.js/filter-object.js
@@ -2,3 +2,6 @@ export function filterObject(obj, predicate) {
   if (obj === null || typeof obj !== "object" || Array.isArray(obj)) {
     throw new TypeError("filterObject: obj debe ser un objeto simple");
   }
+  if (typeof predicate !== "function") {
+    throw new TypeError("filterObject: predicate debe ser una función");
+  }
