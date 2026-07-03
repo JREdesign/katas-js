0905905c0a9007416ce31650feb9da4e28e0ce2e
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-03 17:10:26 +0200

    Add tests for normalizeWhitespace function

--- src/01-strings/normalize-whitespace2.test.js
+++ src/01-strings/normalize-whitespace2.test.js
@@ -1,3 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { normalizeWhitespace } from "./normalize-whitespace.js";
 
+describe("normalizeWhitespace", () => {
+  it("sustituye varios espacios por uno solo", () => {
+    expect(normalizeWhitespace("hola    mundo")).toBe("hola mundo");
+  });
