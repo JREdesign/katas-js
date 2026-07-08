4b779c621b71e6fafd76780c05d62808ccbde788
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-08 18:18:04 +0200

    Validate input types in partition function
    Add type checks for input parameters in partition function.

--- src/02-arrays/partition.js
+++ src/02-arrays/partition.js
@@ -2,3 +2,8 @@ export function partition(arr, predicate) {
   if (!Array.isArray(arr)) {
     throw new TypeError("partition: arr debe ser un array");
   }
+  if (typeof predicate !== "function") {
+    throw new TypeError(
+      "partition: predicate debe ser una función"
+    );
+  }
