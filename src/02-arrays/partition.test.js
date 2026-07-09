2c6c43ca874aa685f472da4a744f9afd7e637f62
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-09 11:16:47 +0200

    Add test for partition function

--- src/02-arrays/partition.test.js
+++ src/02-arrays/partition.test.js
@@ -1,2 +1,10 @@
 import { describe, expect, it } from "vitest";
 import { partition } from "./partition.js";
+
+describe("partition", () => {
+  it("separa los elementos según una condición", () => {
+    expect(partition([1, 2, 3, 4], number => number % 2 === 0)).toEqual([
+      [2, 4],
+      [1, 3]
+    ]);
+  });
