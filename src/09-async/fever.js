0ede09d43a7c32708e71c29ff2315b457dc788f5
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-12 10:19:03 +0200

    Add asynchronous data retrieval function
    Refactor code to introduce asynchronous data retrieval with a promise.

--- src/09-async/fever.js
+++ src/09-async/fever.js
@@ -1,24 +1,5 @@
-function sumar(a, b) {
-  return a + b;
-}
-
-const resultado = sumar(5, 3);
-console.log(resultado); // 8
-
-function mostrarResultado(numero1, numero2) {
-  try {
-    const resultado = sumar(numero1, numero2);
-
-    console.log(`${numero1} + ${numero2} = ${resultado}`);
-
-    return resultado;
-  } catch (error) {
-    console.error(`No se pudo realizar la suma: ${error.message}`);
-  
-    return null;
-  }
-}
-
-const primeraSuma = mostrarResultado(5, 3);
-const segundaSuma = mostrarResultado(10, 7);
-const sumaIncorrecta = mostrarResultado("5", 4);
+// Simula una operación que tarda 2 segundos
+function obtenerDatos() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const operacionCorrecta = true;
