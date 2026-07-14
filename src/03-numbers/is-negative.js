36eaf5eb8cd6cfde7a828a37a4a4eab88af9d07f
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-14 17:13:11 +0200

    Add input validation to isNegative function
    Throw TypeError for invalid input types in isNegative function.

--- src/03-numbers/is-negative.js
+++ src/03-numbers/is-negative.js
@@ -1,2 +1,6 @@
 export function isNegative(value) {
   if (typeof value !== "number" || Number.isNaN(value)) {
+    throw new TypeError(
+      "isNegative: value debe ser un number válido"
+    );
+  }
