22b5551aa19573974f79d347cd8f1fbafa704182
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-20 11:43:44 +0200

    Add tests for renameKey function

--- src/06- objects.js/rename-key.test.js
+++ src/06- objects.js/rename-key.test.js
@@ -1,3 +1,10 @@
 import { describe, expect, it } from "vitest";
 import { renameKey } from "./rename-key.js";
 
+describe("renameKey", () => {
+  it("cambia el nombre de una propiedad", () => {
+    expect(renameKey({ name: "Jorge", age: 30 }, "name", "username")).toEqual({
+      username: "Jorge",
+      age: 30
+    });
+  });
