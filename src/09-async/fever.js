685ea3f87a4976960ac103f1a92476474be8d56b
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-24 23:47:54 +0200

    Clean up JSDoc comments in HttpError class
    Removed unnecessary JSDoc comments from HttpError class.

--- src/09-async/fever.js
+++ src/09-async/fever.js
@@ -1,11 +1,4 @@
 class HttpError extends Error {
-  /**
-   * Crea un error relacionado con una petición HTTP.
-   *
-   * @param {string} message Mensaje descriptivo del error.
-   * @param {number} status Código de estado HTTP.
-   * @param {string} url URL en la que ocurrió el error.
-   */
   constructor(message, status, url) {
     super(message);
 
