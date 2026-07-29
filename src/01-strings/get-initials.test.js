83d72ed09dfc60de5699cc201d064fb01e955820
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-07-29 19:17:38 +0200

    Add test for getInitials function

--- src/01-strings/get-initials.test.js
+++ src/01-strings/get-initials.test.js
@@ -1,3 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { getInitials } from "./get-initials.js";
 
+describe("getInitials", () => {
+  it("obtiene las iniciales de un nombre", () => {
+    expect(getInitials("Jorge García")).toBe("JG");
+  });
