1c57035e7bc61f9c046cbe88cb5ef0d77d19651c
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-08-11 13:25:49 +0200

    Add test for countOccurrences function

--- src/02-arrays/count-occurrences.test.js
+++ src/02-arrays/count-occurrences.test.js
@@ -1,2 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { countOccurrences } from "./count-occurrences.js";
+
+describe("countOccurrences", () => {
+  it("cuenta las apariciones de un número", () => {
+    expect(countOccurrences([1, 2, 1, 3, 1], 1)).toBe(3);
+  });
