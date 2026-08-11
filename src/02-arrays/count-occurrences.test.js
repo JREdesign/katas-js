8630a9a24fa68001f53ea19a00d78781ff6af29d
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-08-11 13:29:16 +0200

    Add test for counting NaN occurrences

--- src/02-arrays/count-occurrences.test.js
+++ src/02-arrays/count-occurrences.test.js
@@ -8,3 +8,6 @@ describe("countOccurrences", () => {
   it("cuenta las apariciones de un string", () => {
     expect(countOccurrences(["a", "b", "a"], "a")).toBe(2);
   });
+  it("puede contar valores NaN", () => {
+    expect(countOccurrences([NaN, 1, NaN], NaN)).toBe(2);
+  });
