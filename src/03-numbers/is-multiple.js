705014f0f79565898a9def73dd8e38f394415936
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-08-13 11:22:44 +0200

    Implement isMultiple function with type checking
    Add isMultiple function to check integer multiples

--- src/03-numbers/is-multiple.js
+++ src/03-numbers/is-multiple.js
@@ -1,1 +1,6 @@
-
+export function isMultiple(value, divisor) {
+  if (!Number.isInteger(value) || !Number.isInteger(divisor)) {
+    throw new TypeError(
+      "isMultiple: value y divisor deben ser enteros"
+    );
+  }
