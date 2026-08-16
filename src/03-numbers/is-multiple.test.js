2f5ac281c9b7d12a096a74936ab865a84a64b6e1
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-08-16 12:02:10 +0200

    Add tests for non-multiple cases in isMultiple

--- src/03-numbers/is-multiple.test.js
+++ src/03-numbers/is-multiple.test.js
@@ -6,3 +6,8 @@ describe("isMultiple", () => {
     expect(isMultiple(10, 5)).toBe(true);
     expect(isMultiple(12, 3)).toBe(true);
   });
+
+  it("devuelve false cuando el valor no es múltiplo", () => {
+    expect(isMultiple(10, 3)).toBe(false);
+    expect(isMultiple(7, 2)).toBe(false);
+  });
