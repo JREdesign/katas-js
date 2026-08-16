424cddb9a80c3ece069e7c24ad606635faad845c
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-08-16 11:51:13 +0200

    Add tests for isMultiple function
    Add tests for isMultiple function to check multiples

--- src/03-numbers/is-multiple.test.js
+++ src/03-numbers/is-multiple.test.js
@@ -1,3 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { isMultiple } from "./is-multiple.js";
 
+describe("isMultiple", () => {
+  it("devuelve true cuando el valor es múltiplo", () => {
+    expect(isMultiple(10, 5)).toBe(true);
+    expect(isMultiple(12, 3)).toBe(true);
+  });
