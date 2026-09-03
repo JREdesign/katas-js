841387714ac6f91034830f110236158114651b4d
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-09-03 12:50:26 +0200

    Fix duplicate return statement in kebabToCamel
    Removed duplicate return statement in kebabToCamel function.

--- src/01-strings/kebab-to-camel.js
+++ src/01-strings/kebab-to-camel.js
@@ -2,8 +2,6 @@ export function kebabToCamel(input) {
   if (typeof input !== "string") {
     throw new TypeError("kebabToCamel: input debe ser un string");
   }
-  return input.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
-}
-
+  
   return input.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
 }
