d1a5777f7d2b3dd0ecc37ffee832538312c544bf
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-09-04 12:45:13 +0200

    Add tests for kebabToCamel function
    Add tests for kebabToCamel function to verify conversion from kebab-case to camelCase.

--- src/01-strings/kebab-to-camel.test.js
+++ src/01-strings/kebab-to-camel.test.js
@@ -1,3 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { kebabToCamel } from "./kebab-to-camel.js";
 
+describe("kebabToCamel", () => {
+  it("convierte kebab-case a camelCase", () => {
+    expect(kebabToCamel("hello-world")).toBe("helloWorld");
+    expect(kebabToCamel("background-color")).toBe("backgroundColor");
+  });
