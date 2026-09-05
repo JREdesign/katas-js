96fa4f32891d3921c503e576f1b5e6d208ac0fe1
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-09-05 18:25:11 +0200

    Validate input to groupConsecutive function
    Add validation to ensure all values are integers.

--- src/11-group-consecutive/group2.js
+++ src/11-group-consecutive/group2.js
@@ -2,3 +2,7 @@ export function groupConsecutive(numbers) {
   if (!Array.isArray(numbers)) {
     throw new TypeError("groupConsecutive: numbers debe ser un array");
   }
+
+  if (!numbers.every(Number.isInteger)) {
+    throw new TypeError("groupConsecutive: todos los valores deben ser enteros");
+  }
