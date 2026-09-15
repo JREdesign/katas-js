bc010e1fc466cd12c508f4e7374a6d92f8ffc1c8
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-09-15 11:08:54 +0200

    Add test for moving item backwards in array

--- src/02-arrays/move-item.test.js
+++ src/02-arrays/move-item.test.js
@@ -9,3 +9,11 @@ describe("moveItem", () => {
       "a"
     ]);
   });
+
+  it("mueve un elemento hacia atrás", () => {
+    expect(moveItem(["a", "b", "c"], 2, 0)).toEqual([
+      "c",
+      "a",
+      "b"
+    ]);
+  });
