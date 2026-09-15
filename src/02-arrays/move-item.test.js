cfbef7fe17f9b4896df65a8d7340e024726d0883
Jorge A. Revuelta Eugercios <153644115+JREdesign@users.noreply.github.com>
2026-09-15 11:07:17 +0200

    Add test for moving an item forward in array

--- src/02-arrays/move-item.test.js
+++ src/02-arrays/move-item.test.js
@@ -1,2 +1,11 @@
 import { describe, expect, it } from "vitest";
 import { moveItem } from "./move-item.js";
+
+describe("moveItem", () => {
+  it("mueve un elemento hacia delante", () => {
+    expect(moveItem(["a", "b", "c"], 0, 2)).toEqual([
+      "b",
+      "c",
+      "a"
+    ]);
+  });
